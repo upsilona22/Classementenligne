109c90ac78eb97cfd6bc9e8eae3078fdd711937d
upsilona22 <123645115+upsilona22@users.noreply.github.com>
2023-02-08 18:46:55 +0100

    Update downloadData.js

--- netlify/functions/downloadData.js
+++ netlify/functions/downloadData.js
@@ -1,6 +1,6 @@
 const fetch = require('isomorphic-fetch');
 const Dropbox = require('dropbox').Dropbox;
-let dbx = new Dropbox({accessToken: "sl.BYdbDNDqd932RdVnoqEvXRYTZ3OO__OGK8EvCyHluvfzTIbkZNQLAqiHdu1zd581HjPxhyNe-ufL2MrDp274utu34xpvmrqXLG4MX6oTpWDsi1pX5cRDKXpKNN3umI-TGljOS9W4Xcct", fetch: fetch});
+let dbx = new Dropbox({accessToken: "sl.BYe6ezkhiutjw7QXMR7Lv2mNuqm5-2f_-9v4ayUP3lzrA25z5Fo2YUnWjN0vqmIByLPA0hapW_OGWDh-k6Jbs_oE5HnflZaD0G4sgcEwDraQpV2_w4Mf0HbsmEFU3ySa9Nf575J_iX2s", fetch: fetch});
 
 exports.handler = async function(event, context) {
     try {
