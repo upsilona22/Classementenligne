866d41c2ba49637993ccf2c5e0e126bc17b8535c
upsilona22 <123645115+upsilona22@users.noreply.github.com>
2023-02-17 19:51:09 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,17 @@ import Header from '@components/Header'
 import LeaderboardTable from '@components/LeaderboardTable'
 const fetch = require('isomorphic-fetch');
 
+function Index({ data, error }) {
+  return (
+    <>
+      {error && <p>{error}</p>}
+      <pre>
+        <code>{JSON.stringify(data, null, 4)}</code>
+      </pre>
+    </>
+  );
+}
+
 export default function Home({leaderboard, totals, time}) {
   return (
     <div className="container">
