842ca6e0ee0fdc8f5901ab2546a30fe6c71ebb74
upsilona22 <123645115+upsilona22@users.noreply.github.com>
2023-02-17 19:10:29 +0100

    Update downloadData.js

--- netlify/functions/downloadData.js
+++ netlify/functions/downloadData.js
@@ -1,7 +1,46 @@
 const fetch = require('isomorphic-fetch');
 const Dropbox = require('dropbox').Dropbox;
-let dbx = new Dropbox({accessToken: "sl.BYeozBE7AiFeuif1Mrdi4S47k5qJeUNmU4Ux-Inw9x594pgxdto5WCaMv9LSrzCOb811Z_6m28U7pbVLNUWLipauh9jes-VOq4KMCKj74N9r3PQW0pVesKuj5wqAegZAM4RIqznNO93g", fetch: fetch});
-//export.handler
+const Dropbox  = require('dropbox').Dropbox;
+const dbx = new Dropbox ({ fetch:fetch, 
+    accessToken: "sl.BZBrN2NF536u74pzo3_HLwoyO4rwnJNVWGU5TJ0dn75hdj_P2MK9dcqgnDfI0iqDnl4ExmJg4Xsy8NVNIf4VhxHmQEm884wpsvH03ceQ7pVleTzMCrQwkiuzj40V4bS7ntfqEG1g5oQT", 
+    clientId: ["6q53aq0206qd2de"],
+    clientSecret: 'chbvq8t7rj2ijun',
+    refreshToken: 'F-csXUU3j2IAAAAAAAAAAV-sJC8LtkSeI64riSUcwn5uQt5tmIQhUEg4RQfrNLMX'
+});
+
+const redirectUri = `http://${hostname}:${port}/auth`;
+
+app.get('/', (req, res) => {
+  dbx.auth.getAuthenticationUrl(redirectUri, null, 'code', 'offline', null, 'none', true)
+    .then((authUrl) => {
+      res.writeHead(302, { Location: authUrl });
+      res.end();
+    });
+});
+
+app.get('/auth', (req, res) => { // eslint-disable-line no-unused-vars
+  const { code } = req.query;
+  console.log(`code:${code}`);
+
+  dbx.auth.getAccessTokenFromCode(redirectUri, code)
+    .then((token) => {
+      console.log(`Token Result:${JSON.stringify(token)}`);
+      dbx.auth.setRefreshToken(token.result.refresh_token);
+      dbx.usersGetCurrentAccount()
+        .then((response) => {
+          console.log('response', response);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+app.listen(port);
+
 exports.handler = async function(event, context) {
     try {
         const response = await dbx.filesDownload({path: "/leaderboard.json"});
